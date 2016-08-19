b63ec8cacb8458c3d9bf429b687a328674e7614b
Zaid Humayun <redixhumayun@gmail.com>
2016-08-19 12:40:27 +0530

    Refactored API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,34 +21,37 @@ app.get('/todos', function(req,res){
 //GET /todos/:id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedId;
-	//iterate over todos array looking for a match
-	todos.forEach(function(todo){
-		if(todoId === todo.id){
-			matchedId = todo;
-		}
-	});
+	var matchedId = _.findWhere(todos, {id: todoId});
+
+
+	
 	if(matchedId){
 		res.json(matchedId);
 	}
-	else{
+	else{ 
 		res.status(404).send();
 	}
 
-	//to send 404
-	//res.status(404.send());
 
 	res.send('Asking for todo with id of '+ req.params.id);
 });
 
 app.post('/todos', function(req, res){
-	var body = req.body;
+	//var body = req.body;
+
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	console.log('description: '+body.description);
 
-	res.json(body);
-
 	body.id = todoNextId;
+
+	res.json(body);
 	todoNextId++;
 	todos.push(body);
 });
