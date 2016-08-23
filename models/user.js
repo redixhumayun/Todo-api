fc88fb5f17df2b1c4c60f893fbacb0c01d78367a
Zaid Humayun <redixhumayun@gmail.com>
2016-08-23 15:45:26 +0530

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,27 +1,50 @@
-module.exports = function(sequelize, DataTypes){
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
+module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
-			type: DataTypes.STRING, 
+			type: DataTypes.STRING,
 			allowNull: false,
-			unique: true, 
+			unique: true,
 			validate: {
 				isEmail: true
 			}
 		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
 		password: {
-			type: DataTypes.STRING, 
-			allowNull: false, 
+			type: DataTypes.VIRTUAL,
+			allowNull: false,
 			validate: {
 				len: [7, 100]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashed_password = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashed_password);
 			}
 		}
 	}, {
 		hooks: {
-			beforeValidate: function(user, options){
-				if(typeof user.email === 'string'){
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
 			}
+		},
+		instanceMethods: {
+			toPublicJSON: function(){
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
+			}
 		}
-	})
+	});
 }
